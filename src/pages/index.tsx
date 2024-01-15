d3551fc95b771a5ca9635aae35617355b4aea198
Jake Lee <firstvirtue@gmail.com>
2024-01-15 15:35:05 +0900

    Add key visual

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ import ProjectList from '@/components/ProjectList'
 export default function Home() {
 
   return (
-    <main className="page flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="page flex min-h-screen flex-col items-center justify-between p-24 pt-0">
 
       <KeyVisual />
       <KeyMessage />
--- src/pages/index/KeyVisual.tsx
+++ src/pages/index/KeyVisual.tsx
@@ -1,13 +1,68 @@
+import Link from "next/link"
+
 export default function KeyVisual() {
   const dd = (() => {
     const date = new Date()
 
     return `${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()}`
   })()
-  
+
   return (
     
-    <div className="" style={{'minHeight': '90vh'}}>
+    <>
+    {/* Filter: https://css-tricks.com/gooey-effect/ */}
+    <svg style={{visibility: 'hidden', position: 'absolute'}} width="0" height="0" xmlns="http://www.w3.org/2000/svg" version="1.1">
+        <defs>
+            <filter id="goo"><feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur" />    
+                <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9" result="goo" />
+                <feComposite in="SourceGraphic" in2="goo" operator="atop"/>
+            </filter>
+        </defs>
+    </svg>
+
+      <div className="key-visual w-full max-w-screen-2xl">
+        <div className="flex min-h-screen pt-48 pb-24 gap-6">
+          <div className="relative visual w-2/3 pt-32">
+            <div className="absolute top-0 text-8xl font-bold italic"
+            style={{
+              'letterSpacing': '-0.02em',
+            }}
+            >
+              The best of my work
+            </div>
+
+            <Link href={'/work/samsung-fund'} className="block relative h-full rounded-2xl overflow-hidden p-8 pt-20"
+            style={{'backgroundColor': '#9EBED7'}}>
+              <video className="w-full h-full object-cover rounded-2xl shadow-xl" src="/prj.mp4" autoPlay loop muted playsInline
+              style={{'viewTransitionName': ''}}
+              ></video>
+
+            <div className="absolute top-12 left-16 z-10">
+            {/* <svg className="w-7" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><path d="m100,0H0v100C0,44.77,44.77,0,100,0Z" fill="#F9F8F6"></path></svg> */}
+              <div className="goo">
+                React를 이용한
+                데이터 핸들링!<br />
+                삼성자산운용 통합 플랫폼
+              </div>
+              {/* <svg className="w-7" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><path d="m100,0H0v100C0,44.77,44.77,0,100,0Z" fill="#F9F8F6"></path></svg> */}
+            </div>
+            </Link>
+          </div>
+          <div className="flex flex-col gap-6 w-1/3">
+
+            <div className="h-2/4 rounded-2xl overflow-hidden"
+            style={{'backgroundColor': '#B8D2D8', 'width': '84%'}}
+            >
+            
+            </div>
+
+            <div className="h-2/4 rounded-2xl overflow-hidden"
+            style={{'backgroundColor': '#83936F'}}>
+              <img className="w-full h-full object-cover" src="/iconic20.png" alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
 
       <div style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', pointerEvents: 'none' }}>
         <a href="https://pmnd.rs/" style={{ position: 'absolute', bottom: 40, left: 90, fontSize: '13px', color: '#777' }}>
@@ -25,6 +80,6 @@ export default function KeyVisual() {
         </div>
       </div>{' '}
 
-    </div>
+    </>
   )
 }
