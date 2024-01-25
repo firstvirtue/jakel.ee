9b611536a9a8c37a811e5853d9551bc3b183e3b7
Sang Oh Lee <sangohlee@Sangs-MacBook-Pro.local>
2024-01-25 23:59:31 +0900

    Update slogan

--- src/pages/index/KeyMessage.tsx
+++ src/pages/index/KeyMessage.tsx
@@ -1,3 +1,6 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react'
+
 export default function KeyMessage() {
   return (
     <>
@@ -13,11 +16,39 @@ export default function KeyMessage() {
     </p>
 
     <p className='slogan'>
+    <span 
+      css={{
+        display: 'block',
+        fontSize: '2rem',
+        fontWeight: '400',
+        marginBottom: '5rem',
+        whiteSpace: 'nowrap',
+      }}
+    >
+    I want to touch people with my art. <span css={{fontSize: '3rem', fontWeight: 100, marginLeft: '3rem', marginRight: '3rem', }}>+</span>
+    I want to touch people with my art. <span css={{fontSize: '3rem', fontWeight: 100, marginLeft: '3rem', marginRight: '3rem', }}>+</span>
+    I want to touch people with my art. <span css={{fontSize: '3rem', fontWeight: 100, marginLeft: '3rem', marginRight: '3rem', }}>+</span>
+    </span>
       {/* I want to touch people<br/> with my art.<br/>  */}
+      "
       I want them to say:<br/> 
       He feels deeply,<br/> 
-      He feels tenderly.<br/>
-      {/* Through my work.<br/> */}
+      He feels tenderly."<br/>
+      <span css={{display: 'block', marginTop: '2rem', textAlign: 'right', fontSize: '2rem'}}>- Loving Vincent</span>
+
+      <span 
+      css={{
+        display: 'block',
+        fontSize: '2rem',
+        fontWeight: '400',
+        marginTop: '5rem',
+        whiteSpace: 'nowrap',
+      }}
+    >
+    I want to touch people with my art. <span css={{fontSize: '3rem', fontWeight: 100, marginLeft: '3rem', marginRight: '3rem', }}>+</span>
+    I want to touch people with my art. <span css={{fontSize: '3rem', fontWeight: 100, marginLeft: '3rem', marginRight: '3rem', }}>+</span>
+    I want to touch people with my art. <span css={{fontSize: '3rem', fontWeight: 100, marginLeft: '3rem', marginRight: '3rem', }}>+</span>
+    </span>
     </p>
     </>
   )
