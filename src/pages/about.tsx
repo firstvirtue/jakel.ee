696665edd7e47a012ac9ba5ccbe8238eb51007c3
Jake Lee <firstvirtue@gmail.com>
2024-02-02 00:19:16 +0900

    Add about contents

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,5 +1,47 @@
 export default function About() {
   return (
-    <>About..</>
+    <>
+    <div className="max-w-3xl mr-auto ml-auto mt-64">
+      <h1 className="text-7xl font-bold uppercase">About</h1>
+      <hr />
+
+      <h2 className="text-3xl mt-10">Who</h2>
+      <p>
+        지난 10여 년이 조금 넘는 기간 동안 UI 프론트엔드 개발 분야에서 삼성, 현대, 기아와 같은 국내 대형 클라이언트의 웹 프로젝트들이 우수한 퀄리티의 결과물로 현실화 되게 하는 것이 저의 책임이었습니다.
+      </p>
+      <p>
+        
+      </p>
+      <br />
+      <p>
+        제가 속해있는 UI 프론트엔드 세계는 제가 커리어를 시작할 당시 RIA(Silverlight, Flash 등) 라는 이름으로 불리던 무렵부터 지금까지 정말 많은 변화가 소용돌이처럼 끊임없이 지나오고 있습니다.
+      </p>
+      <p>
+        이 흥미로운 기술 세계에서 저는 생존을 위한 과감한 취사선택과 최신 기술 스택을 지향하며 변화를 꾀했고, 놀랍게도 열정적이고 변화를 두려워하지 않는 한 사람이 되었습니다.
+      </p>
+      <p>
+        React로 대표되는 현재의 기술 스택과 더불어 다가올 공간컴퓨팅 세상에 Web이 어떻게 표현될지를 지금도 계속해서 고민하고 있습니다. 기술은 스토리텔링이 있고 그렇기에 예언적이라고 생각합니다.
+      </p>
+      <br />
+      <p>
+        저에 대해 흥미로운 부분이 있다면 언제든지 연락해 주세요.
+      </p>
+      <p>
+        무언가 멋진 것들을 함께 만들어 봅시다!
+      </p>
+
+      
+      <h2 className="text-3xl mt-10">Experiences</h2>
+      <div>- 2023</div>
+      <div>- 2022</div>
+      <div>...</div>
+
+
+      <h2 className="text-3xl mt-10">Awards</h2>
+      <div>- 2023</div>
+      <div>i-awards final winner</div>
+      <div>...</div>
+    </div>
+    </>
   )
 }
