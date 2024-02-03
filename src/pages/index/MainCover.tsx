4b99a2ba2dc92ac251c6c46d182874edb27c7303
Jake Lee <firstvirtue@gmail.com>
2024-02-03 21:30:24 +0900

    Update data

--- src/pages/index/MainCover.tsx
+++ src/pages/index/MainCover.tsx
@@ -44,7 +44,7 @@ export default function MainCover() {
 
               {/* <div id="key-visual" className="w-full h-full object-cover rounded-2xl shadow-xl"></div> */}
 
-            <div className="absolute top-12 left-16 z-10">
+            <div className="absolute top-12 left-16 z-10" css={{pointerEvents: 'none'}}>
             {/* <svg className="w-7" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><path d="m100,0H0v100C0,44.77,44.77,0,100,0Z" fill="#F9F8F6"></path></svg> */}
               <div className="goo">
                 React를 이용한
