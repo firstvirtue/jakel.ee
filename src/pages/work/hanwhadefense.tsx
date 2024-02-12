699bb7df9e15bdddaffa8a48455c667c6eb72d08
Jake Lee <firstvirtue@gmail.com>
2024-02-13 02:13:34 +0900

    Add en hanwhadefense

--- src/pages/work/hanwhadefense.tsx
+++ src/pages/work/hanwhadefense.tsx
@@ -22,7 +22,7 @@ import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 export default function Post(
   _props: InferGetServerSidePropsType<typeof getServerSideProps>
 ) {
-  
+  const { t } = useTranslation(['common', 'second-page'])
   const currentProject = getProjectInformation('hanwhadefense')
   const prevProject = getProjectInformation('lotteshopping')
   const nextProject = getProjectInformation('hankooktire')
@@ -37,12 +37,12 @@ export default function Post(
 
           <h2 className="text-5xl font-bold text-center">Invaluable path for better world</h2>
 
-          <h3>실무 레벨의 3D 에셋</h3>
+          <h3>{t('second-page:projects.hanwhadefense.h01')}</h3>
           <p>
-            그 동안 개인 작업으로 샘플 3D를 만져본 적은 많았지만 이렇게 본격적으로 실무 프로젝트를 경험해 보니 감회가 새로웠습니다.
+          {t('second-page:projects.hanwhadefense.p01')}
           </p>
           <p>
-            이전 한국타이어 때의 3D 모델이 비교적 경량의 단순한 모델이었던 반면 이 쇼룸은 모델도 많아지고 그에 따른 요건이 조금 더 복잡해져 이전보다 더 많은 챌린지가 있었습니다.
+          {t('second-page:projects.hanwhadefense.p02')}
           </p>
 
           <figure data-size="lg">
@@ -50,7 +50,7 @@ export default function Post(
           </figure>
 
           <p className="standalone">
-            포인트 클릭, UI 연동을 넘어 에셋의 분할 로드, 뷰 전환, 애니메이션, 스프라이트 처리 등 꽤 다양한 3D 기술과 UI 기법을 적용할 수 있었던 알찬 프로젝트 였습니다! 
+          {t('second-page:projects.hanwhadefense.p03')}
           </p>
 
           <figure data-size="lg">
