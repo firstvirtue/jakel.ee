7145cbf5748e50bb0b78d0246cedd78faecc5f77
Jake Lee <firstvirtue@gmail.com>
2024-02-13 12:36:12 +0900

    Add en about

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,3 +1,4 @@
+/** @jsxImportSource @emotion/react */
 import type {
   GetServerSideProps,
   InferGetServerSidePropsType,
@@ -5,116 +6,134 @@ import type {
 import type { GetStaticProps, InferGetStaticPropsType } from 'next'
 import { useTranslation, Trans, i18n } from 'next-i18next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import { css } from "@emotion/react"
 
 
 export default function About(
   _props: InferGetServerSidePropsType<typeof getServerSideProps>
 ) {
+  const { t } = useTranslation(['common', 'second-page'])
+  
+  const tit = css({
+    marginBottom: '1rem'
+  })
+  const awardClient = css({
+    opacity: 0.4
+  })
+
   return (
     <>
     <div className="max-w-3xl mr-auto ml-auto mt-64">
       <h1 className="text-7xl font-bold uppercase">About</h1>
       <hr />
 
-      <h2 className="text-3xl mt-10">What I Do</h2>
+      <h2 className="block text-3xl mt-10 mb-3">What I Do</h2>
       <p>
-        지난 10여 년간 UI 프론트엔드 개발 분야에서 삼성, 현대, 기아와 같은 국내 대형 클라이언트의 웹 프로젝트들이 우수한 품질의 결과물로 현실화 되게 하는 것이 저의 책임이었습니다.
+      {t('common:about.what-i-do-01')}
       </p>
-      <p>
-        
-      </p>
+
       <br />
       <p>
-        제가 속해있는 UI 프론트엔드 세계는 제가 커리어를 시작할 당시 RIA라는 이름으로 불리던 무렵부터 지금까지 정말 많은 변화가 소용돌이처럼 지나오고 있습니다.
+      {t('common:about.what-i-do-02')}
       </p>
       <p>
-        이 흥미로운 기술 세계에서 때로는 생존을 위해 과감히 가진 것을 버리고 최신 기술을 선택해야 할 때가 있었습니다.
+      {t('common:about.what-i-do-03')}
       </p>
       <p>
-        가지고 있는 기술을 폐기하고 그 자리에 새로운 것들을 채우는 과정은 매우 고통스러웠지만
+      {t('common:about.what-i-do-04')}
       </p>
       <p>
-        이 과정은 기술이, 특히 UI/프론트엔드 파트가 어떤 라이브러리에도 종속적이지 않고 본질은 동일하다는 통찰을 얻게 하였습니다.
+      {t('common:about.what-i-do-05')}
       </p>
       <p>
-        스크린에 컴포넌트를 올리고 이를 여러 방향으로 연결시키고 그 상태를 관리하고 게임과 같은 고수준 애니메이션이 필요하다면 렌더링 루프로 처리하는 등 이 모든 일이 같으며, 라이브러리는 이를 보다 효율적으로 만들어 주는 도구에 불과함을 말입니다.
-      </p>
-      <p>
-        또 이런 기술과 프로젝트 여정 속에서 만났던 뛰어난 재능을 가진 사람들과 함께
-      </p>
-      <p>
-        문제를 고민하고 때로는 밤새 씨름하며 해결하면서 놀랍게도 열정적이고 단단하며 팀에 헌신적인 사람들을 발견하게 되었습니다.
+      {t('common:about.what-i-do-06')}
       </p>
       <br />
 
-      <h2 className="text-3xl mt-10"></h2>
-      <p>전 다음 부분에 남다른 강점이 있습니다.</p>
-      <h3>UX Engineering</h3>
-      <p>모션, 인터랙션, 3D 등 프론트엔드에서도 프리젠테이션 레이어에 보다 더 특화되어 있고 디자이너와 협업하는 것을 좋아합니다. 기능적/심미적으로 아름다운 어플리케이션 개발의 가치를 이해하고 구현할 자신이 있습니다.</p>
-      <h3>Semi Full stack</h3>
-      <p>DB 테이블을 설계하고 API를 만들어 서비스하며, 쌓여진 데이터를 통계 쿼리로 분석한 경험이 있습니다. 웹의 전반적인 구조와 흐름을 알고 있다는 점은 제게 훌륭한 자산입니다.</p>
+      <p css={{marginBottom: '0.4em'}}>{t('common:about.strengths-01')}</p>
+      <h3 className='font-bold mt-3'>{t('common:about.strengths-02')}</h3>
+      <p>{t('common:about.strengths-03')}</p>
+      <h3 className='font-bold mt-3'>{t('common:about.strengths-04')}</h3>
+      <p>{t('common:about.strengths-05')}</p>
       <br />
       <p>
-        지금은 주력으로 사용하는 React를 최적화하고 보다 빠르게 만들고자 고민합니다
-        더불어 당면한 AI 시대와 특히 다가올 공간 컴퓨팅 세상에 Web이 어떻게 표현될지 생각합니다.
+      {t('common:about.conclusion-01')}
       </p> 
       <br />
 
-      <h2 className="text-3xl mt-10"></h2>
       <p>
-        저에 대해 흥미로운 부분이 있다면 언제든지 연락해 주세요.
+      {t('common:about.conclusion-02')}
       </p>
       <p>
-        새로운 아이디어에 기여할 준비가 되어 있습니다.
+      {t('common:about.conclusion-03')}
       </p>
       <p>
-        무언가 멋진 것들을 함께 만들어 봅시다!
+      {t('common:about.conclusion-04')}
       </p>
       
-      <h2 className="text-3xl mt-10">Experience</h2>
-      <div>2021 - 2023</div>
-      <div>DOES Interactive</div>
-      <div>2017 - 2021</div>
-      <div>Iropke</div>
-      <div>2017 - 2017</div>
-      <div>Ninefive</div>
-      <div>2016 - 2017</div>
-      <div>Duknsoft</div>
-      <div>2012 - 2016</div>
-      <div>Eloicube</div>
-      <div>2008 - 2010</div>
-      <div>Flur</div>
+      <h2 className="block text-3xl mt-10 mb-3">Experience</h2>
+      <dl css={{ 
+        display: 'flex',
+        flexWrap: 'wrap',
+        'dt': {
+          width: '20%'
+        },
+        'dd': {
+          width: '80%'
+        }
+      }}>
+        <dt>2021 - 2023</dt><dd>DOES Interactive</dd>
+        <dt>2017 - 2021</dt><dd>Iropke</dd>
+        <dt>2017 - 2017</dt><dd>Ninefive</dd>
+        <dt>2016 - 2017</dt><dd>Duknsoft</dd>
+        <dt>2012 - 2016</dt><dd>Eloicube</dd>
+        <dt>2008 - 2010</dt><dd>Flur</dd>
+      </dl>
 
 
-      <br />
-      <br />
-      <div className="text-4xl">
-      Years
-      13
+      <div className="text-5xl mt-20 mb-20" css={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        'strong': {
+          textAlign: 'center',
+          fontWeight: 400,
+        },
+        'span': {
+          display: 'block',
+          marginTop: '1rem',
+          color: '#E06D72'
+        }
+      }}>
+        <strong>
+        Years <br/>
+        <span>13</span>
+        </strong>
       
-      Projects
-      23+
+        <strong>
+        Projects <br/>
+        <span>23+</span>
+        </strong>
 
-      Awards
-      13
+        <strong>
+        Awards <br/>
+        <span>13</span>
+        </strong>
       </div>
 
-      <h2 className="text-3xl mt-10">Awards</h2>
-      <div>2023 앤어워드 과학기술정보통신부 장관상 - <span>한화에어로스페이스 플랫폼</span></div>
-      <div>2023 앤어워드 Grand Prix - <span>삼성자산운용 통합 플랫폼</span></div>
-      <div>Web Award Korea 2023 최고대상 - <span>삼성자산운용 통합 플랫폼</span></div>
-
-      <div>Red Dot Design Award 2023 - <span>한국타이어 디지털 마케팅&세일즈 플랫폼</span></div>
-      <div>iF DESIGN AWARD 2023 - <span>한국타이어 디지털 마케팅&세일즈 플랫폼</span></div>
-      <div>2022 앤어워드 Grand Prix - <span>한국타이어 글로벌 플랫폼</span></div>
-      <div>2022 앤어워드 Grand Prix - <span>스마일게이트</span></div>
-      <div>Web Award Korea 2022 최고대상 - <span>기아 월드와이드</span></div>
-
-      <div>Web Award Korea 2022 제조업분야 대상 - <span>한국타이어 글로벌 플랫폼</span></div>
-      <div>Red Dot Design Award 2022 - <span>내 손안에 Galaxy Studio</span></div>
-      <div>Web Award Korea 2019 최우수상 - <span>한화 갤러리아 공식 웹사이트</span></div>
-      <div>Web Award Korea 2019 최우수상 - <span>갤러리아 디지털 커뮤니케이션 채널 통합</span></div>
-      <div>Web Award Korea 2019 최우수상 - <span>비비드 스튜디오</span></div>
+      <h2 className="block text-3xl mt-10 mb-3">Awards</h2>
+      <div>{t('common:about.award-010')} - <span css={awardClient}>{t('common:about.award-011')}</span></div>
+      <div>{t('common:about.award-020')} - <span css={awardClient}>{t('common:about.award-021')}</span></div>
+      <div>{t('common:about.award-030')} - <span css={awardClient}>{t('common:about.award-031')}</span></div>
+      <div>{t('common:about.award-040')} - <span css={awardClient}>{t('common:about.award-041')}</span></div>
+      <div>{t('common:about.award-050')} - <span css={awardClient}>{t('common:about.award-051')}</span></div>
+      <div>{t('common:about.award-060')} - <span css={awardClient}>{t('common:about.award-061')}</span></div>
+      <div>{t('common:about.award-070')} - <span css={awardClient}>{t('common:about.award-071')}</span></div>
+      <div>{t('common:about.award-080')} - <span css={awardClient}>{t('common:about.award-081')}</span></div>
+      <div>{t('common:about.award-090')} - <span css={awardClient}>{t('common:about.award-091')}</span></div>
+      <div>{t('common:about.award-100')} - <span css={awardClient}>{t('common:about.award-101')}</span></div>
+      <div>{t('common:about.award-110')} - <span css={awardClient}>{t('common:about.award-111')}</span></div>
+      <div>{t('common:about.award-120')} - <span css={awardClient}>{t('common:about.award-121')}</span></div>
+      <div>{t('common:about.award-130')} - <span css={awardClient}>{t('common:about.award-131')}</span></div>
     </div>
     </>
   )
@@ -130,7 +149,7 @@ export const getServerSideProps: GetServerSideProps<Props> = async ({
   props: {
     ...(await serverSideTranslations(locale ?? 'en', [
       'second-page',
-      'footer',
+      'common',
     ])),
   },
 })
